fc2477aa4700abc7a1ed7997db00e40ec3ea4c05
JamesTheCode31 <jsn8340@students.uc-bcf.edu.ph>
2024-09-06 08:46:50 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+/**
+ *
+ * @author m303user
+ */
+/*Anna
+September 6, 2024
+ First Programming Session*/
+public class Test {
+    public static void main (String []args){
+        System.out.println("Anna \t\tRhodora \nQuitaleg");
+        //Java Escape characters
+        System.out.print("\n\n\nAnna\"s Class");
+        System.out.println("I Love Java programming");
+         
+    }
+}
